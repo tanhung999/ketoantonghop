223ea3de21e648e0501d552650b9c817b28cc9af
tanhung999 <ngotanhung02011999@gmail.com>
2023-12-05 21:01:40 +0700

    fix post phieunhap

--- src/phieunhaphanghoa/phieunhaphanghoa.service.ts
+++ src/phieunhaphanghoa/phieunhaphanghoa.service.ts
@@ -82,6 +82,7 @@ export class PhieunhaphanghoaService {
                     tPhieuNhapHangHoaChiTiet :{
                         create: {
                             ...insertPhieuNhapHangHoaChiTietData,
+                            nMaSo
                         }
                     }
                 }
